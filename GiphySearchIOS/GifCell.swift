a33e1310b66a05e36ff10a6fe9844287b84fd132
Henry Calderon <theceintiant@gmail.com>
2020-04-01 21:34:37 -0700

    Completed Ch 3. Outlets and Delegates

---
+++ GiphySearchIOS/GifCell.swift
@@ -0,0 +1,21 @@
+//
+//  GifCell.swift
+//  GiphySearchIOS
+//
+//  Created by Henry Calderon on 4/1/20.
+//  Copyright © 2020 Henry Calderon. All rights reserved.
+//
+
+import UIKit
+class GifCell: UITableViewCell {
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        // Add views
+    }
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+    }
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+    }
+}
