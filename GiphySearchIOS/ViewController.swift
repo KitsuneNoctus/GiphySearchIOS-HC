31c335cd50dea8213406b3959e6223903aeacb76
Henry Calderon <theceintiant@gmail.com>
2020-04-01 21:10:04 -0700

    Initial Commit

---
+++ GiphySearchIOS/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GiphySearchIOS
+//
+//  Created by Henry Calderon on 4/1/20.
+//  Copyright © 2020 Henry Calderon. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
